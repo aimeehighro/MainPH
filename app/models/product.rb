c17aae287fad77d7c741d882b3fb8357175cb0a1
Aimee <mmalaluan6@academic.rrc.ca>
2019-11-07 00:45:44 -0600

    Installed Active Storage

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,7 +5,9 @@ class Product < ApplicationRecord
   has_many :ordered_products
   has_many :orders, through: :ordered_products
 
-  validates :name, :price, :stock_quantity, :image, :description, presence: true
+  validates :name, :price, :stock_quantity, :description, presence: true
   validates :price, :stock_quantity, numericality: true
   validates :stock_quantity, numericality: { only_integer: true }
+
+  has_one_attached :image
 end
