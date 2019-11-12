5aa901891fd60aadae3cca6be5e4405c616d98d3
Aimee <mmalaluan6@academic.rrc.ca>
2019-11-12 15:20:22 -0600

    Added pagination and partial on the products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 class ProductsController < ApplicationController
   def index
+    @pagy, @products = pagy(Product.all, items: 8)
   end
 
   def show
+    @product = Product.find(params[:id])
   end
 end
