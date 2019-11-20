079ebb6cfd1b6784a4906df9758d6261fa634dc6
Aimee <mmalaluan6@academic.rrc.ca>
2019-11-19 22:09:25 -0600

    Added pagination on categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,7 +4,23 @@ class CategoriesController < ApplicationController
   def index
     @categories = Category.all
   end
+
   def show
     @category = Category.find(params[:id])
+    @pagy, @products = pagy(Product.where(category_id: @category.id), items: 8)
   end
+  # def show
+  #   # @pagy, @products = if params[:product]
+  #   #                      pagy(Product.where('name LIKE ?', "%#{params[:genre]}%"), items: 5)
+  #   #                    else
+  #   #                      pagy(Product.all, items: 5)
+  #   # end
+  #   @categories = Category.find(params[:id])
+  #   # @products = Product.where(category_id: @categories.id)
+  #   @pagy, @products = if params[:categories]
+  #                        pagy(Product.where('name LIKE ?', "%#{params[:product]}%"), items: 5)
+  #                      else
+  #                        pagy(Product.all, items: 5)
+  #   end
+  # end
 end
