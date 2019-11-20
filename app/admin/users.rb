6159869950ffacab8360abaf9466e17a7ff5349f
Aimee <mmalaluan6@academic.rrc.ca>
2019-11-20 00:22:36 -0600

    Added user model on the active admin

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,11 +1,14 @@
-ActiveAdmin.register User do
+# frozen_string_literal: true
 
+ActiveAdmin.register User do
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at
+  permit_params :email, :encrypted_password, :reset_password_token,
+                :reset_password_sent_at, :remember_created_at, :first_name,
+                :last_name, :street, :city, :postal_code, :province_id
   #
   # or
   #
@@ -14,5 +17,4 @@ ActiveAdmin.register User do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
-  belongs_to :provinces
+  # belongs_to :province
+  validates :first_name, :last_name, :street, :city, :postal_code, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
