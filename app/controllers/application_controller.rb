7f9d259f92eb723882d8f698daa883138c2a02ea
Aimee <mmalaluan6@academic.rrc.ca>
2019-11-20 00:23:25 -0600

    Customized devise sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
+# frozen_string_literal: true
+
 class ApplicationController < ActionController::Base
   include Pagy::Backend
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name street city area postal_code province_id])
+    # devise_parameter_sanitizer.for(:account_update) << :name
+  end
 end
