2cf26b165a913057fab0b5332de1fbd26fb02a3e
Aimee <mmalaluan6@academic.rrc.ca>
2019-11-20 00:55:44 -0600

    Add references on province (User)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  # belongs_to :province
+  belongs_to :province
   validates :first_name, :last_name, :street, :city, :postal_code, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
