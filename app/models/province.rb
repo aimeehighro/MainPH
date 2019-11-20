d012faeeedbf84a470764e5a1c7a9ecb1b9afa90
Aimee <mmalaluan6@academic.rrc.ca>
2019-11-19 22:29:47 -0600

    Removed customer association and added user association

--- app/models/province.rb
+++ app/models/province.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Province < ApplicationRecord
-  has_many :customers
+  has_many :users
   validates :name, presence: true
   validates :name, uniqueness: true
   validates :GST, :PST, :QST, :HST, numericality: true
