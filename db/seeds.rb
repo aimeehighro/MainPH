3f2089d2adce299716d7f58a3748f8f54a2d0064
Aimee <mmalaluan6@academic.rrc.ca>
2019-11-21 13:08:47 -0600

    Made product display prettier and added imagemagick

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@mainph.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+# AdminUser.create!(email: 'admin@mainph.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+require 'open-uri'
+
+store_url = 'https://elevatedfaith.com'
+# hoodie_url = store_url + '/collections/christian-hoodies'
+# hoodie_url = store_url + '/collections/christian-t-shirts'
+# hoodie_url = store_url + '/collections/tank-tops'
+# hoodie_url = store_url + '/collections/kids'
+hoodie_url = store_url + '/collections/christian-bracelets'
+
+hoodies_html = open(hoodie_url).read
+hoodies_doc = Nokogiri::HTML(hoodies_html)
+
+hoodies_selector = 'div.grid-product__content'
+hoodies_table = hoodies_doc.css(hoodies_selector)
+
+# hoodies = Category.find(4)
+# puts hoodie.name
+
+hoodies_table.each do |hoodie|
+  hoodie_name = hoodie.at_css('div.grid-product__title--body').content
+  hoodie_price = hoodie.at_css('span.money').text
+  hoodies.products.build(name: hoodie_name, price: hoodie_price[1..6], stock_quantity: 99).save
+  # puts hoodie_name
+  # puts hoodie_price[1..6]
+end
