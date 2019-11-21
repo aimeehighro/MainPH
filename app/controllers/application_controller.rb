9d28021555e236797c5bb27c86f282f270c543c6
Aimee <mmalaluan6@academic.rrc.ca>
2019-11-20 21:38:59 -0600

    Users can edit their information

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name street city area postal_code province_id])
-    # devise_parameter_sanitizer.for(:account_update) << :name
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name last_name street city area postal_code province_id])
   end
 end
