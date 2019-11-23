0b202bcd0bc447c8e26aafe7c7568a96e3373542
Aimee <mmalaluan6@academic.rrc.ca>
2019-11-23 13:40:15 -0600

    Remove Order associations from customers and payments

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,8 +3,6 @@
 class Order < ApplicationRecord
   has_many :ordered_products
   has_many :products, through: :ordered_products
-  has_many :payments
-  has_many :customers, through: :payments
 
   validates :status, presence: true
 end
