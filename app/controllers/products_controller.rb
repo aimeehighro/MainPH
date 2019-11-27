a94052092223357f76c4521201a136873d8c5fce
Aimee <mmalaluan6@academic.rrc.ca>
2019-11-26 19:05:23 -0600

    Refactor based on rubocop standard

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,18 +1,30 @@
 # frozen_string_literal: true
 
 class ProductsController < ApplicationController
+  before_action :fetch_products, only: [:index]
+  before_action :sort_products, only: [:index]
   def index
-    @pagy, @products = if params[:search]
+    @pagy, @products = if params[:search] || params[:category_id]
                          pagy(Product.search(params[:search],
                                              params[:category_id]), items: 4)
                        elsif params[:sort]
-                         pagy(Product.order("#{params[:sort]} desc"), items: 4)
+                         sort_products
                        else
-                         pagy(Product.all, items: 8)
+                         fetch_products
                        end
   end
 
   def show
     @product = Product.find(params[:id])
+  end
+
+  private
+
+  def sort_products
+    @pagy, @products = pagy(Product.order("#{params[:sort]} desc"), items: 4)
+  end
+
+  def fetch_products
+    @pagy, @products = pagy(Product.all, items: 8)
   end
 end
